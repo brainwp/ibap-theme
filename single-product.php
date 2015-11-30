a678eec02f9f73d5c93af8c16be600923787cdbd
Diego Rojas <diego@brasa.art.br>
2015-11-30 21:18:23 -0200

    substituido templates so single-product

---
+++ single-product.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header( 'matuck' ); ?>
+<div class="ibap-single col-md-12">
+
+	<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
+	<header class="entry-header col-md-12">
+	<div class="container">
+
+
+
+	</div>
+	</header><!-- .entry-header -->
+
+
+
+	<div class="clear"></div>
+<div class="container">
+	<div class="container col-md-10">
+			<main id="main-content" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php wc_get_template_part( 'content', 'single-product' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+
+			</main><!-- #main -->
+	</div><!-- .container -->
+
+	<?php
+		/**
+		 * woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
+
+</div>
+</div>
+<?php
+get_footer(); ?>
